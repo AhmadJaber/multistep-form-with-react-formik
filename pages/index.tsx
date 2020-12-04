c9849f606f596098f46ea8da46404373352addfe
AhmadJaber <jaber131706@gmail.com>
2020-12-04 07:29:19 +0600

    :tada: initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, CardContent } from "@material-ui/core";
+
+export default function Home() {
+  return (
+    <Card>
+      <CardContent>
+        <h3>hello</h3>
+      </CardContent>
+    </Card>
+  );
+}
